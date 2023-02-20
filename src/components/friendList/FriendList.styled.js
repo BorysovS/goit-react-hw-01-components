47d61ec8e2d76617db82a941d2af053baa6a8e22
BorysovS <s.borisovbar@gmail.com>
2023-02-20 23:57:03 +0200

    fix

--- src/components/friendList/FriendList.styled.js
+++ src/components/friendList/FriendList.styled.js
@@ -6,11 +6,17 @@ export const List = styled.ul`
   margin-left: auto;
   margin-right: auto;
   margin-bottom: 24px;
+  padding: 24px;
   display: flex;
   flex-direction: column;
   gap: 10px;
   max-width: 250px;
   background-color: #fff;
+  border: 1px solid black;
+  border-radius: 10px;
+  background-color: #dce6e5;
+  box-shadow: inset 0 -3em 3em rgba(0, 0, 0, 0.1), 0 0 0 2px rgb(255, 255, 255),
+    0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
   transition: transform 0.5s cubic-bezier(0.4, 0, 0.2, 1);
   :hover {
     transform: scale(1.05);
@@ -23,6 +29,7 @@ export const ListItem = styled.div`
   gap: 10px;
   padding: 10px;
   width: 100%;
+  background-color: white;
   box-shadow: 0px 1px 3px rgb(0 0 0 / 12%), 0px 1px 1px rgb(0 0 0 / 14%),
     0px 2px 1px rgb(0 0 0 / 20%);
   border-radius: 0 0 4px 4px;
